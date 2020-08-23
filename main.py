bbc8a2ae18c6499c9f3e435add2b4077d0de10d6
rossi <d.rossberg@elco-automation.de>
2020-08-23 15:49:01 +0200

    fix

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 # from RNN.time_series_prediction.Sensor import *
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 from Sensor import Sensors
 
 import os
 # configuration
 save_info = 1       # 1: save information in file, 0: do not save
-train = 0           # 1: train model, 0: load model
+train = 1           # 1: train model, 0: load model
 
 n_lag = 1
 n_epochs = 100
 dataset_path = os.path.join(os.curdir, 'dataset','csv', 'sampled')
-root_path = '/home/PNW/wu1114/Dropbox/US_Steel/USS-RF-Fan-Data-Analytics/_13_Preliminary-results/LSTM-preciction/multi-step-prediction/compare-v3/'
+root_path = '/content/Predictive-Maintenance'
 sensor_names = {
     'MAIN_FILTER_IN_PRESSURE','MAIN_FILTER_OIL_TEMP','MAIN_FILTER_OUT_PRESSURE','OIL_RETURN_TEMPERATURE',
     'TANK_FILTER_IN_PRESSURE','TANK_FILTER_OUT_PRESSURE','TANK_LEVEL','TANK_TEMPERATURE','FT-202B',
-    'FT-204B','PT-203','PT-204'
+    'FT-204B','PT-203','PT-20 4'
 }
 sample_rates_n_seq = {
     'sample_1_hour':(1,48), 'sample_6_hour':(1,8), 'sample_12_hour':(1,4),
